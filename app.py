57c29cb518bdeecd49933de928e6ece116cc2fd0
Derek-Wds <dingsuwang@yahoo.com>
2018-04-10 00:55:52 +0800

    latest

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = '\x1b\x9d\xa8\x9b\xbbn\xa5\xfd\xd2\xa4\x16%{c\xba~\xd
 
 @app.route('/')
 def test():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/home/')
 def homepage():
@@ -54,4 +54,5 @@ def register_page():
 
 
 if __name__ == '__main__':
-    app.run()
+    print("I Fucked Up")
+    app.run(host="0.0.0.0", port=8080)
