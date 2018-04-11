2c3e28d093c532fb6ab571808bbf4dd95617e8b0
Derek-Wds <dingsuwang@yahoo.com>
2018-04-12 01:07:31 +0800

    little change

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app.config['SECRET_KEY'] = '\x1b\x9d\xa8\x9b\xbbn\xa5\xfd\xd2\xa4\x16%{c\xba~\xd
 def home_page():
     return render_template("index.html")
 
+@app.route('/home/')
+def log_home_page():
+    return render_template("home.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
