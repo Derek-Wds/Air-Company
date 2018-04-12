450bfc47d412d4423ad166cee345c75de163a96e
Derek-Wds <dingsuwang@yahoo.com>
2018-04-12 21:18:26 +0800

    fancy stuff

--- app.py
+++ app.py
@@ -9,11 +9,23 @@ app.config['SECRET_KEY'] = '\x1b\x9d\xa8\x9b\xbbn\xa5\xfd\xd2\xa4\x16%{c\xba~\xd
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("test.html")
 
-@app.route('/home/')
-def log_home_page():
-    return render_template("home.html")
+@app.route('/home/customer/')
+def customer_page():
+    return render_template("chome.html")
+
+
+@app.route('/home/agent/')
+def agent_page():
+    return render_template("ahome.html")
+
+
+@app.route('/home/staff/')
+def staff_page():
+    return render_template("shome.html")
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -32,7 +44,7 @@ def login_page():
             password = request.form['password']
 
             if email == 'a@qq.com' and password == '1':
-                return redirect(url_for('homepage'))
+                return redirect(url_for('customer_page'))
             else:
                 err = "Account does not exist or password error!"
                 flash(err)
