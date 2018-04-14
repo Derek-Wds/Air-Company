d5989b69e34fadcb80a4943e857fbfe5deb749bf
Masaki Kagesawa <mk5986@nyu.edu>
2018-04-14 11:58:47 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import pymysql.cursors
-
-DB = {'host': 'dbairline.cjqsdkeisvcl.us-west-2.rds.amazonaws.com', 'port': 3306,
-                         'user':'root',
-                         'password':'deyairline',
-                         'db':'airline',
-                         'charset': 'utf8',
-                         'cursorclass': pymysql.cursors.DictCursor}
